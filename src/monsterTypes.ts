8e62bcd6b29f9e6cfb9ff68923712215a45c6595
pjkbsn <philip.jkbsn@gmail.com>
2024-04-24 10:46:12 +0200

    added types

---
+++ src/monsterTypes.ts
@@ -0,0 +1,21 @@
+import monsters from "./monsters.json";
+
+export type MonsterType = {
+  Id: number;
+  First_name: string;
+  Last_name: string;
+  Class: string;
+  Program: string;
+  Description: string;
+};
+
+export type MonsterContextType = {
+  currentMonster: MonsterType[];
+  dispatch: React.Dispatch<MonsterAction>;
+};
+
+export type MonsterAction =
+  | { type: "ADD_MONSTER"; payload: MonsterType }
+  | { type: "REMOVE_MONSTER"; payload: number };
+
+export const monsterArray = monsters;
