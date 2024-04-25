eb194a97d7efa46a29b531e8e0f333259bf71db1
pjkbsn <philip.jkbsn@gmail.com>
2024-04-25 11:43:02 +0200

    fixed addmonster action

--- src/components/MonsterReducer/MonsterReducer.tsx
+++ src/components/MonsterReducer/MonsterReducer.tsx
@@ -3,10 +3,10 @@ import { MonsterAction, MonsterType } from "../../monsterTypes";
 export const MonsterReducer = (state: MonsterType[], action: MonsterAction) => {
   switch (action.type) {
     case "ADD_MONSTER":
-      return [];
+      return [...state, action.payload];
     case "REMOVE_MONSTER":
       return state.filter(
-        (monster: MonsterType) => monster.Id !== action.payload
+        (monster: MonsterType) => monster.First_name !== action.payload
       );
     default:
       return state;
