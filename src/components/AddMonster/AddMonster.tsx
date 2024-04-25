8b70b9f2b16f0f84d7009d2f053847275a15c7c3
pjkbsn <philip.jkbsn@gmail.com>
2024-04-25 11:42:27 +0200

    added addmonster

--- src/components/AddMonster/AddMonster.tsx
+++ src/components/AddMonster/AddMonster.tsx
@@ -1,7 +1,96 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../Button/Button";
+import "./AddMonster.scss";
+import { useContext, useState } from "react";
+import { MonsterContext } from "../MonsterContext/MonsterContext";
+
 export const AddMonster = () => {
+  const { dispatch } = useContext(MonsterContext);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [monsterClass, setMonsterClass] = useState("");
+  const [program, setProgram] = useState("");
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
+
+  const addMonster = () => {
+    dispatch({
+      type: "ADD_MONSTER",
+      payload: {
+        First_name: firstName,
+        Last_name: lastName,
+        Class: monsterClass,
+        Program: program,
+        Description: description,
+      },
+    });
+    navigate("/");
+  };
+
+  const homepage = () => {
+    navigate("/");
+  };
+
   return (
-    <>
-      <button>Add Monster</button>
-    </>
+    <div className="AddMonster">
+      <form action="">
+        <div>
+          <label>Name</label>
+          <input
+            placeholder="First"
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            placeholder="Last"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Class</label>
+          <input
+            placeholder="Class"
+            type="text"
+            value={monsterClass}
+            onChange={(e) => setMonsterClass(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Program</label>
+          <input
+            placeholder="Program"
+            type="text"
+            value={program}
+            onChange={(e) => setProgram(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Description</label>
+          <input
+            placeholder="Description"
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className="buttonContainer">
+        <Button buttonName="Homepage" handleClick={homepage} />
+        <Button buttonName="Add monster" handleClick={addMonster} />
+      </div>
+    </div>
   );
 };
+
+// Detta ska monstret innehålla
+//-------------------------------
+//   Id: number;
+//   First_name: string;
+//   Last_name: string;
+//   Class: string;
+//   Program: string;
+//   Description: string;
+//-------------------------------
--- src/pages/MonsterAddPage.tsx
+++ src/pages/MonsterAddPage.tsx
@@ -1,7 +1,10 @@
+import { AddMonster } from "../components/AddMonster/AddMonster";
+
 export const MonsterAddPage = () => {
   return (
     <>
       <h1>MonsterAddPage</h1>
+      <AddMonster />
     </>
   );
 };
